6e837bd33d2d486e60855623b820287e5ad30321
sunshinejoex <sunshinejoex@gmail.com>
2025-05-28 17:38:21 +0800

    状态栏、导航栏api

--- app/src/main/java/com/emo/haha/ext/Views.kt
+++ app/src/main/java/com/emo/haha/ext/Views.kt
@@ -1,9 +1,17 @@
 package com.emo.haha.ext
 
 import android.content.res.Resources
+import android.graphics.Color
+import android.os.Build
 import android.util.TypedValue
 import android.view.View
+import android.view.WindowInsets
 import android.widget.Checkable
+import androidx.annotation.ColorInt
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
 
 /**
  * @author: SunshineJoex
@@ -86,3 +94,69 @@ val Float.sp
     get() = TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_SP, this, Resources.getSystem().displayMetrics
     )
+
+/**
+ * statusBars add topPadding
+ */
+fun View.statusBarsPadding() {
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        val navigationBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        v.setPadding(navigationBars.left, navigationBars.top, navigationBars.right, 0)
+        insets
+    }
+}
+
+
+/**
+ * navigationBarsPadding  add bottomPadding
+ */
+fun View.navigationBarsPadding() {
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        val navigationBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        v.setPadding(navigationBars.left, 0, navigationBars.right, navigationBars.bottom)
+        insets
+    }
+}
+
+/**
+ * statusBars、navigationBarsPadding  add topPadding、bottomPadding
+ */
+fun View.systemBarsPadding() {
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        val navigationBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        v.setPadding(
+            navigationBars.left,
+            navigationBars.top,
+            navigationBars.right,
+            navigationBars.bottom
+        )
+        insets
+    }
+}
+
+/**
+ * darkContent true:暗色字体
+ * decorFitsSystemWindows false:进入状态栏
+ */
+fun FragmentActivity.immersiveStatusBar(
+    darkContent: Boolean = true,
+    decorFitsSystemWindows: Boolean = true,
+    @ColorInt color: Int = Color.TRANSPARENT
+) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) { // Android 15+
+        window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+            val statusBarInsets = insets.getInsets(WindowInsets.Type.statusBars())
+            view.setBackgroundColor(color)
+            // Adjust padding to avoid overlap
+            view.setPadding(0, statusBarInsets.top, 0, 0)
+            insets
+        }
+    } else {
+        // For Android 14 and below
+        window.statusBarColor = color
+    }
+    val insertControllerCompat = WindowCompat.getInsetsController(window, window.decorView)
+    WindowCompat.setDecorFitsSystemWindows(window, decorFitsSystemWindows)
+    insertControllerCompat.isAppearanceLightStatusBars = darkContent
+}
+
